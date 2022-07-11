97c5bd6b30740eccca44ad836ce6f44bd2c9aced
Mirosław Gucwa <mgucwa@gmail.com>
2022-07-11 22:51:35 +0200

    Products - add CRUD

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -17,7 +17,6 @@ router.get('/employees/random', (req, res) => {
 });
 
 router.get('/employees/:id', (req, res) => {
-  console.log(req.params.id);
   req.db.collection('employees').findOne({ _id: ObjectId(req.params.id) }, (err, data) => {
     if (err) res.status(500).json({ message: err });
     else if (!data) res.status(404).json({ message: 'Not found' });
