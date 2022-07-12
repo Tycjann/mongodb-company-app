b73080663147b1e8b4fc47586c908d5660573f02
Mirosław Gucwa <mgucwa@gmail.com>
2022-07-12 10:12:48 +0200

    Add Mongoose in Products

--- models/product.model.js
+++ models/product.model.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const productsSchema = new mongoose.Schema({
   name: { type: String, required: true },
   client: { type: String, required: true },
-});
+},
+  {
+    // versionKey: '_somethingElse' 
+    versionKey: false
+  });
 
 module.exports = mongoose.model('Product', productsSchema);
