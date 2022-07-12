0b57259e1d57ca015332fdfc3548879731307e15
Mirosław Gucwa <mgucwa@gmail.com>
2022-07-12 10:54:11 +0200

    Fix update i delete department

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -61,10 +61,12 @@ router.post('/departments', async (req, res) => {
 router.put('/departments/:id', async (req, res) => {
   const { name } = req.body;
   try {
-    const dep = await Department.findById(req.params.id);
+    let dep = await Department.findById(req.params.id);
     if (dep) {
       await Department.updateOne({ _id: req.params.id }, { $set: { name: name } });
-      res.json({ message: 'OK' });
+      // res.json({ message: 'OK' });
+      dep = await Department.findById(req.params.id);
+      res.json(dep);
     }
     else res.status(404).json({ message: 'Not found...' });
   }
@@ -81,7 +83,8 @@ router.delete('/departments/:id', async (req, res) => {
     const dep = await Department.findById(req.params.id);
     if (dep) {
       await Department.deleteOne({ _id: req.params.id });
-      res.json({ message: 'OK' });
+      // res.json({ message: 'OK' });
+      res.json(dep);
     }
     else res.status(404).json({ message: 'Not found...' });
   }
