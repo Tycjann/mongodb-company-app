b426c027e3231a36137d6fab289c2e3173acfea0
Mirosław Gucwa <mgucwa@gmail.com>
2022-07-15 14:11:39 +0200

    Add: Unit test for Employee

--- models/department.model.js
+++ models/department.model.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 
 const departmentSchema = new mongoose.Schema({
   // _id: { type: mongoose.Types.ObjectId, required: true }, // added automatically by MongoDB - always correct
-  name: { type: String, required: true }
+  // name: { type: String, required: true }
+  name: { 
+    type: String, 
+    required: true, 
+    maxlength: 20, 
+    minlength: 5, 
+  }
 });
 
 module.exports = mongoose.model('Department', departmentSchema);
